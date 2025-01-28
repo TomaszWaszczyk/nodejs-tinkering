0e897772d0e479344131aa9cf24a0be2d521fb5b
Tomasz Waszczyk <tomasz@waszczyk.com>
2025-01-28 13:31:50 +0100

    added: makeAdder

--- closure.js
+++ closure.js
@@ -1,9 +1,18 @@
-function outerFunction(outerVariable){
-    return function innerFunction(innerVariable){
-        console.log(`Outer variable: ${outerVariable}`);
-        console.log(`Inner variable: ${innerVariable}`);
-    };
+// function outerFunction(outerVariable){
+//     return function innerFunction(innerVariable){
+//         console.log(`Outer variable: ${outerVariable}`);
+//         console.log(`Inner variable: ${innerVariable}`);
+//     };
+// }
+
+// const closureExample = outerFunction("Hello");
+// closureExample("World");
+
+function makeAdder(x){
+    return function(y){
+        return x+y;
+    }
 }
 
-const closureExample = outerFunction("Hello");
-closureExample("World");
+const adderOf7 = makeAdder(7);
+console.log(adderOf7(3));
