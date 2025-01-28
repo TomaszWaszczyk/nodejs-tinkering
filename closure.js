6988a35bff08b22764bd56dc7d4d2bd5b44ae02e
Tomasz Waszczyk <tomasz@waszczyk.com>
2025-01-28 15:23:50 +0100

    added: private variable in js

--- closure.js
+++ closure.js
@@ -16,3 +16,16 @@ function makeAdder(x){
 
 const adderOf7 = makeAdder(7);
 console.log(adderOf7(3));
+
+// function createCounter() {
+//     let count = 0; // Private variable
+//     return function increment() {
+//         count++;
+//         return count;
+//     };
+// }
+
+// const counter = createCounter();
+// console.log(counter()); // 1
+// console.log(counter()); // 2
+// console.log(counter()); // 3
