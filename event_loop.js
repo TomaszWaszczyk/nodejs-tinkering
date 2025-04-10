d0e667f4a94d09edc7f2d35b3c97ee540730f053
Tomasz Waszczyk <tomasz@waszczyk.com>
2025-04-10 20:12:42 +0200

    add event loop

---
+++ event_loop.js
@@ -0,0 +1,7 @@
+// https://nodejs.org/en/learn/asynchronous-work/event-loop-timers-and-nexttick
+
+setTimeout(() => {
+    const delay = Date.now() - timeoutScheduled;
+  
+    console.log(`${delay}ms have passed since I was scheduled`);
+}, 100);
