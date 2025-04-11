572f8bad6874156c19c7c793f294f0a8ecc11b8d
Tomasz Waszczyk <tomasz@waszczyk.com>
2025-04-11 15:48:13 +0200

    added config.json

--- event_loop.js
+++ event_loop.js
@@ -1,9 +1,26 @@
 // https://nodejs.org/en/learn/asynchronous-work/event-loop-timers-and-nexttick
 
-setTimeout(() => {
-    const delay = Date.now() - timeoutScheduled;
-  
-    console.log(`${delay}ms have passed since I was scheduled`);
-}, 100);
+const http = require('http');
+const fs = require('fs');
 
-console.time('timeout');
+
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+console.log(config);
+
+
+
+// http.createServer((req, res) => {
+//   const data = fs.readFileSync('bigfile.txt'); // 🛑 blocks everything!
+//   res.end(data);
+//   console.log(data);
+// }).listen(3000);
+
+
+
+// setTimeout(() => {
+//     const delay = Date.now() - timeoutScheduled;
+  
+//     console.log(`${delay}ms have passed since I was scheduled`);
+// }, 100);
+
+// console.time('timeout');
