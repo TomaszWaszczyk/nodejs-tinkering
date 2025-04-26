5b7ab4e0c73b968e080b1fd8feff839c36205c3b
Tomasz Waszczyk <tomasz@waszczyk.com>
2025-04-26 17:38:21 +0200

    some bs

--- like.js
+++ like.js
@@ -1,3 +1,12 @@
 export function add(a, b) {
   return a + b;
 }
+
+export function like(array) {
+  if (array.length === 0) {
+    return 'no one likes this';
+  }
+  if(array.length === 1) {
+    return `${array[0]} likes this`;
+  }
+}
--- like.test.js
+++ like.test.js
@@ -1,6 +1,12 @@
 import { test, expect } from "vitest";
-import { add } from "./like.js";
+import { add, like } from "./like.js";
 
 test("adds two numbers", () => {
   expect(add(1, 1)).toBe(2);
 });
+
+test("no one likes this", () => {
+  const result = like([]);
+  expect(result).toBe("no one likes this");
+});
+
