4b62871196370ac91a7e9ff566ef05d79e0dac1a
Tomasz Waszczyk <tomasz@waszczyk.com>
2025-05-27 15:59:29 +0200

    fix

--- asco.mjs
+++ asco.mjs
@@ -12,15 +12,12 @@ function getItemsMaxNumber(items) {
     let maxNumber = 0;
     
     for (let item of items) {
-        // Extract number from the 'name' string using regex
         const match = item.name.match(/\d+/);
         if (match) {
             const number = parseInt(match[0]);
             if (number > maxNumber) {
                 maxNumber = number;
             }
-        }else {
-          maxNumber = 0;
         }
     }
     console.log(`The biggest number is: ${maxNumber}`);
