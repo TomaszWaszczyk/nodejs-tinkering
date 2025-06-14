70ab855d16eba930d1abd3ae375cb262d325c4b0
Tomasz Waszczyk <tomasz@waszczyk.com>
2025-06-14 23:36:39 +0200

    added data types tinkering

---
+++ data_types.mjs
@@ -0,0 +1,8 @@
+console.log(typeof 42);        // "number"
+console.log(typeof "hello");   // "string"
+console.log(typeof true);      // "boolean"
+console.log(typeof undefined); // "undefined"
+console.log('type of null:', typeof null);      // "object" (this is a historical quirk)
+console.log(typeof Symbol());  // "symbol"
+console.log(typeof {});        // "object"
+console.log(typeof []);        // "object"
