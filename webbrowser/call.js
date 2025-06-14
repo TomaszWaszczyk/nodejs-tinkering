3671f29224fe8772bec2e0e3dd414b5bb7b46517
Tomasz Waszczyk <tomasz@waszczyk.com>
2025-06-14 18:39:04 +0200

    call experiment

--- webbrowser/call.js
+++ webbrowser/call.js
@@ -2,8 +2,13 @@ function test() {
     return this;
 }
 
+function greeting(){
+    console.log(`Hello, my name is ${this.name}`)
+}
+
 var greet = {
     name: 'Tomek',
 };
 
-console.log(test.call(greet)); // { name: 'Tomek' }
+// console.log(test.call(greet)); // { name: 'Tomek' }
+greeting.call(greet);
