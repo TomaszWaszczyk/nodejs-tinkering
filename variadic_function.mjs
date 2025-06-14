0ff0913502cb4c53e13f1e1fe271438dd3278dd8
Tomasz Waszczyk <tomasz@waszczyk.com>
2025-06-14 22:00:24 +0200

    call variadic function

--- variadic_function.mjs
+++ variadic_function.mjs
@@ -1,6 +1,10 @@
+let array = [1,2,3,4,5];
+
 function sum(...args) {
     return args.reduce((acc, curr) => acc + curr, 0);
 }
 
 console.log(sum(1,2,3,4,5,6))
 console.log(sum());
+console.log('array', array) // nope
+console.log(sum(...array));
