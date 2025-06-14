7e8cb2f2df7df4fe7337159602f9640bcb6c873d
Tomasz Waszczyk <tomasz@waszczyk.com>
2025-06-14 23:44:34 +0200

    added falsy values

---
+++ falsy_values.mjs
@@ -0,0 +1,8 @@
+if (false) { /* not executed */ }
+if (0) { /* not executed */ }
+if ("") { /* not executed */ }
+if (null) { /* not executed */ }
+if (undefined) { /* not executed */ }
+if (NaN) { /* not executed */ }
+if (0n) { /* not executed */ }
+if (document.all) { /* not executed */ }
